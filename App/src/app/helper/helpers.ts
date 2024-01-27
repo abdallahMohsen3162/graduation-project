074746dca151c0a96fc8b5207dbddd3738c46c21
Abdallah Mohsen <93709847+abdallahMohsen3162@users.noreply.github.com>
2024-01-27 23:48:25 +0200

    Update helpers.ts

--- App/src/app/helper/helpers.ts
+++ App/src/app/helper/helpers.ts
@@ -7,3 +7,16 @@ const setCookie = (name:string, value:any, daysToLive:number) => {
  const deleteCookie = (name:string) => {
    document.cookie = `${name}=; Max-Age=0; path=/`;
  };
+
+
+const getCookie = (name:string) => {
+  const cDecode = decodeURIComponent(document.cookie);
+  const cArray = cDecode.split("; ");
+  let ret ;
+  cArray.forEach((el, idx) => {
+    if(el.indexOf(name) == 0){
+      ret = el.substring(name.length + 1);
+    }
+  })
+  return ret;
+}
