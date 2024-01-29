16b3b1ce771ded89a91461537400052dfe8ebaae
Abdallah Mohsen <93709847+abdallahMohsen3162@users.noreply.github.com>
2024-01-29 22:12:48 +0200

    Update Reciever.jsx

--- App/src/app/components/Reciever.jsx
+++ App/src/app/components/Reciever.jsx
@@ -1,11 +1,15 @@
 "use client"
 import React, { useState } from 'react';
+import axios from 'axios';
 const videoExtensions = ['.mp4', '.webm', '.avi'];
 const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
-
+let arr = [''];
 function detectType(name){
   console.log(name);
   let lstDot = name.lastIndexOf('.');
+  if(lstDot < 0){
+    return "Can't determine"
+  }
   console.log(lstDot);
   let ex = name.substring(lstDot)
   console.log(ex);
@@ -19,12 +23,14 @@ function detectType(name){
 }
 
 function UploadPage() {
+  const [image, setImage] = useState(null);
   const [fileType, setFileType] = useState(null);
   const [filename, setfilename] = useState("");
 
   function handleFileUpload(event) {
     const file = event.target.files[0];
     setfilename(file.name);
+    setImage(file);
     if (file) {
       console.log(detectType(file.name))
     }
@@ -37,11 +43,33 @@ function UploadPage() {
   function handleDrop(event) {
     event.preventDefault();
     const file = event.dataTransfer.files[0];
+    setImage(file);
     setfilename(file.name);
     if (file) {
       console.log(detectType(file.name))
     }
   }
+
+
+  const handleUpload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('image', image);
+
+      const response = await axios.post('http://127.0.0.1:80/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      console.log(response.data);
+      arr = response.data.message;
+      setc(c + 1);
+      console.log(arr);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
@@ -55,6 +83,7 @@ function UploadPage() {
         <button onClick={() => document.querySelector('input[type="file"]').click()}>Browse</button>
         {fileType && <p>File type: {filename}</p>}
       </div>
+      <button onClick={handleUpload}>Upload Image</button>
     </div>
   );
 }
